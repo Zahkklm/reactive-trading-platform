1675f6699ca2b55b6a9635afa5b7f9c9256f6474
Özgür Peynirci <ozgurpeynirci@gmail.com>
2025-06-01 18:42:44 +0300

    Initialize project with Reactive Web, H2 and R2DBC (for Postgres driver), Spring Boot DevTools and Lombok dependencies.

---
+++ src/test/java/com/ozgur/customerportfolio/CustomerportfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozgur.customerportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerportfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
