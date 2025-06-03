3910f5dae2305569f39a733daa95d4b7e81b95bd
Özgür Peynirci <ozgurpeynirci@gmail.com>
2025-06-03 07:10:22 +0300

    Add CustomerPortfolioService for trading order

---
+++ aggregator-service/aggregator-service/src/main/java/com/ozgur/aggregator/service/CustomerPortfolioService.java
@@ -0,0 +1,40 @@
+package com.ozgur.aggregator.service;
+
+import com.ozgur.aggregator.client.CustomerServiceClient;
+import com.ozgur.aggregator.client.StockServiceClient;
+import com.ozgur.aggregator.dto.*;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CustomerPortfolioService {
+
+    private final StockServiceClient stockServiceClient;
+    private final CustomerServiceClient customerServiceClient;
+
+    public CustomerPortfolioService(StockServiceClient stockServiceClient, CustomerServiceClient customerServiceClient) {
+        this.stockServiceClient = stockServiceClient;
+        this.customerServiceClient = customerServiceClient;
+    }
+
+    public Mono<CustomerInformation> getCustomerInformation(Integer customerId) {
+        return this.customerServiceClient.getCustomerInformation(customerId);
+    }
+
+    public Mono<StockTradeResponse> trade(Integer customerId, TradeRequest request) {
+        return this.stockServiceClient.getStockPrice(request.ticker())
+                                      .map(StockPriceResponse::price)
+                                      .map(price -> this.toStockTradeRequest(request, price))
+                                      .flatMap(req -> this.customerServiceClient.trade(customerId, req));
+    }
+
+    private StockTradeRequest toStockTradeRequest(TradeRequest request, Integer price) {
+        return new StockTradeRequest(
+                request.ticker(),
+                price,
+                request.quantity(),
+                request.action()
+        );
+    }
+
+}
